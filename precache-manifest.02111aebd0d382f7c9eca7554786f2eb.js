ef894558741982acb6a30f543977a2ae3b769b17
unknown <mukherjeerivu7@gmail.com>
2020-07-29 11:05:18 +0530

    Updates

---
+++ precache-manifest.02111aebd0d382f7c9eca7554786f2eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47821326d5bc57d03b3623c583f4056",
+    "url": "/github-jobs-react/index.html"
+  },
+  {
+    "revision": "dcf5a67ac4ae2207ff60",
+    "url": "/github-jobs-react/static/js/2.f2b61ec9.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs-react/static/js/2.f2b61ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd50f84cf3d7b1bfa95",
+    "url": "/github-jobs-react/static/js/main.72175a2f.chunk.js"
+  },
+  {
+    "revision": "b1d8b9e5a24bfa1b523c",
+    "url": "/github-jobs-react/static/js/runtime-main.ded4c08c.js"
+  }
+]);
